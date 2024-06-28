ec34b253279c9a52525b9b149fa838a2d80227e3
Keyur <78588580+keyur155@users.noreply.github.com>
2024-06-29 02:00:49 +0530

    Delete reward_app directory

--- reward_app/routes/HomeRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/UserController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-// router.get('/home', authMiddleware, UserController.getProfile);
-// router.put('/', authMiddleware, UserController.updateProfile);
-
-module.exports = router;
